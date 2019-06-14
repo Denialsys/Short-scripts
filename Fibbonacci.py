4c47f39ed831eed35f96854f5ec52ff72d633fec
Denialsys <denialsys@gmail.com>
2019-06-14 14:45:32 +0800

    Update fibbonacci

--- Fibbonacci.py
+++ Fibbonacci.py
@@ -2,11 +2,12 @@
 
 
 def fibbonacci(numOfTerms, retVal = "sequence"):
-    '''Fibbonacci function
+    '''Calculates the fibonacci sequence
+
     Params:
         numOfTerms => Number term to be calculated
         retVal => The desired return value of a function
-                  'sequence' -> Prints the sequence
+                  'sequence' -> Prints the sequence (default)
                   'number' -> The last value of calculated term'''
     
     fibboNum = 0
@@ -15,11 +16,19 @@ def fibbonacci(numOfTerms, retVal = "sequence"):
     
     if numOfTerms <= 0: print ("Please enter a positive integer")
 
-    elif numOfTerms == 1: print (0)
+    elif numOfTerms == 1:
+        if retVal == "sequence": print (0)
+        else: return 0
 
     else:
         for iterationCount in range(numOfTerms):
-            
+
+            ##Check the parameter retVal
+            if retVal == "sequence": print (fibboNum, end = ' ')
+            elif (retVal == "number" and iterationCount == numOfTerms -1 ):
+                return fibboNum
+
+            ##Adjust values
             nthTerm = fibboNum + nextTerm
             fibboNum = nextTerm
             nextTerm = nthTerm
