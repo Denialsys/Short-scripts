ec33085cbd512dda49f63fc0fb708498a2bef38e
Denialsys <denialsys@gmail.com>
2019-06-20 18:14:14 +0800

    bullshit indent error

--- time hr min sec.py
+++ time hr min sec.py
@@ -1,29 +1,35 @@
 '''Time given a the seconds count'''
 
 def returnTime(pSeconds, pIsMilitary):
-	'''Returns a tupple containing Hour minute and second
-        @Param pSeconds -> time in seconds format
-        @Param pIsMilitary -> format the return into 24hr or 12hr'''
-
 	second = pSeconds % 60
 	minute = (pSeconds // 60) % 60
 	hourMilitary = (pSeconds // 3600) % 24
-	return (hourMilitary, minute, second)
-
         if pIsMilitary:
                 return (hourMilitary, minute, second)
-        
-        else:                
+        else:
                 meridiem = "AM"
                 if hourMilitary == 0:
-                        
                         hourStandard = 12
                 elif hourMilitary > 12:
-                        
                         hourStandard = hourMilitary - 12
                         meridiem = "PM"
                 else:
                         hourStandard = hourMilitary
-                        
                 return (hourStandard, minute, second, meridiem)
 
+##def returnTime(pSeconds, pIsMilitary):
+##        second = pSeconds % 60
+##        minute = (pSeconds // 60) % 60
+##        hourMilitary = (pSeconds // 3600) % 24
+##        if pIsMilitary:
+##                return (hourMilitary, minute, second)
+##        else:
+##                meridiem = "AM"
+##                if hourMilitary == 0:
+##                        hourStandard = 12
+##                elif hourMilitary > 12:
+##                        hourStandard = hourMilitary - 12
+##                        meridiem = "PM"
+##                else:
+##                        hourStandard = hourMilitary
+##                return (hourStandard, minute, second, meridiem)
