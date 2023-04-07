195bc6a755d0d1b2b1e84762675584e8eb638ae1
Jenver I <37936529+Denialsys@users.noreply.github.com>
2023-04-07 22:24:23 +0800

    Added snipped screen capture

--- screencapture_grabber/main.py
+++ screencapture_grabber/main.py
@@ -1,6 +1,7 @@
 from PIL import ImageGrab, Image
 import ctypes
 import time
+import winsound
 
 user32 = ctypes.windll.user32
 isTriggered = lambda triggerKey: user32.GetKeyState(triggerKey) > 1
@@ -8,11 +9,13 @@ isTriggered = lambda triggerKey: user32.GetKeyState(triggerKey) > 1
 # Key code for printscreen
 VK_F10 = 0x79
 VK_SNAPSHOT = 0x2C
+VK_P = 0x50
 
 # For debouncing
 is_key_pressed = {
     VK_SNAPSHOT: False,
-    VK_F10: False
+    VK_F10: False,
+    VK_P: False
 }
 
 is_watchdog_loop_enabled = True
@@ -20,14 +23,48 @@ is_watchdog_loop_enabled = True
 file_format = '.png'
 output_directory = 'D:\\Desktop'
 file_sequence_name = 'screen_capture'
-indexer = 0
 
-def save_clipboard_img(indexer):
+img_coord_stack = []
+
+class POINT(ctypes.Structure):
+    _fields_ = [("x", ctypes.c_long), ("y", ctypes.c_long)]
+
+
+def queryMousePosition():
+    pt = POINT()
+    ctypes.windll.user32.GetCursorPos(ctypes.byref(pt))
+    return { "x": pt.x, "y": pt.y}
+
+
+def snip_screen_capture():
+    pos = queryMousePosition()
+    if len(img_coord_stack) < 1:
+        img_coord_stack.append(pos)
+
+    else:
+        img_coord_stack.append(pos)
+        screen_capture = ImageGrab.grab()
+        cropped = screen_capture.crop((
+            img_coord_stack[0]['x'],
+            img_coord_stack[0]['y'],
+            img_coord_stack[1]['x'],
+            img_coord_stack[1]['y']
+        ))
+
+        img_coord_stack.clear()
+        save_clipboard_img(cropped)
+        winsound.MessageBeep(winsound.MB_OK)
+
+
+def save_clipboard_img(img=None):
     time.sleep(.08)
-    img = ImageGrab.grabclipboard()
-    file_name = f'{output_directory}\\{file_sequence_name}{indexer}{file_format}'
+    if img is None:
+        img = ImageGrab.grabclipboard()
+
+    file_name = f'{output_directory}\\{file_sequence_name}{time.time_ns()}{file_format}'
     img.save(file_name)
     print(f'{file_name} created')
+
 
 print('Starting the watchdog')
 while(is_watchdog_loop_enabled):
@@ -35,8 +72,7 @@ while(is_watchdog_loop_enabled):
         # Screen Capture
         if isTriggered(VK_SNAPSHOT):
             if not is_key_pressed[VK_SNAPSHOT]:
-                save_clipboard_img(indexer)
-                indexer += 1
+                save_clipboard_img()
             is_key_pressed[VK_SNAPSHOT] = True
         else:
             is_key_pressed[VK_SNAPSHOT] = False
@@ -48,6 +84,14 @@ while(is_watchdog_loop_enabled):
             is_key_pressed[VK_F10] = True
         else:
             is_key_pressed[VK_F10] = False
+
+        # Snip screen capture
+        if isTriggered(VK_P):
+            if not is_key_pressed[VK_P]:
+                snip_screen_capture()
+            is_key_pressed[VK_P] = True
+        else:
+            is_key_pressed[VK_P] = False
 
     except Exception as e:
         print(e.args)
